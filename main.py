09cd518f8c57e3de85701fc00fc1f879e52819ec
potato-stack <hatrungquyen89@gmail.com>
2022-12-31 14:45:34 +0700

    script for simulate node

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-print("Xin chào ThingsBoard")
+# print("Xin chào ThingsBoard")
 import paho.mqtt.client as mqttclient
 import time
 import json
+import sys
 
 BROKER_ADDRESS = "thingsboard.cloud"
 PORT = 1883
-THINGS_BOARD_ACCESS_TOKEN = "U4ugcpWt538wp5AwVGJ0"
-
+THINGS_BOARD_ACCESS_TOKEN = "PkSXWNyACzrxEtvM9Blv"
+state = "On"
 
 def subscribed(client, userdata, mid, granted_qos):
     print("Subscribed...")
+    temp_data = {'value': 'On','active': 'true'}
+    client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
 
 
 def recv_message(client, userdata, message):
     print("Received: ", message.payload.decode("utf-8"))
-    temp_data = {'value': True}
+    global state
+    temp_data = {'value': 'On','active': 'true'}
     try:
         jsonobj = json.loads(message.payload)
-        if jsonobj['method'] == "setValue":
-            temp_data['value'] = jsonobj['params']
+        if jsonobj['method'] == "Set sensor" and jsonobj['params'] == "On":
+            temp_data['value'] = "On"
+            temp_data['active'] = 'true'
+            state = "On"
             client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
+        elif jsonobj['method'] == "Set sensor" and jsonobj['params'] == "Off":
+            temp_data['value'] = "Off"
+            temp_data['active'] = 'false'
+            client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
+            state = "Off"
     except:
         pass
 
@@ -43,26 +54,63 @@ client.on_message = recv_message
 
 f = open(r'C:\Users\hatru\OneDrive\Desktop\data\tram_1.txt','r')
 while f:
-    data = f.readline().replace('\n', '')
-    t = data.split(";")
-    m = "{"
-    for x in range(len(t) - 1):
-        t1 = t[x].split(',')
-        if x != len(t) - 1 and x != 0:
-            m = m + ", ";
-        if len(t1) > 1:
-            m = m + '"' + t1[0] + '"' + ': ' + '"' + t1[1] + '"'
-        else:
-            if t1[0].find(':') != -1:
-                m = m + '"Hour": '
+    if state == "On":
+        data = f.readline().replace('\n', '')
+        t = data.split(";")
+        # m = "{"
+        entry_dict = {
+                "SEQ": "",
+                "STT": "",
+                "Hour": "",
+                "Date": "",
+                "pH": "",
+                # "PH": "",
+                # "COLOR": "",
+                "Color": "",
+                #"FLOW"
+                "Flow": "",
+                # "FLOW": "",
+                "TSS": "",
+                # "TEMP": "",
+                "temperature": "",
+                "COD": "",
+                "CLO": "",
+                "SS": "",
+                "TN": "",
+                "NH4": "",
+                "N-NH4": "",
+                "N-NH4+": "",
+                "MO": "",
+                "VBAT": "",
+                "VDDA": "",
+                "INTEMP": "",
+            }
+        for x in range(len(t) - 1):
+            t1 = t[x].split(',')
+            # if x != len(t) - 1 and x != 0:
+            #     m = m + ", "
+            if len(t1) > 1:
+                # m = m + '"' + t1[0] + '"' + ': ' + '"' + t1[1] + '"'
+                if t1[0] == "COLOR":
+                    entry_dict["Color"] = t1[1]
+                elif t1[0] == "PH":
+                    entry_dict["pH"] = t1[1]
+                elif t1[0] == "FLOW":
+                    entry_dict["Flow"] = t1[1]
+                elif t1[0] == "TEMP" or t1[0] == "Temp":
+                    entry_dict["temperature"] = t1[1]
+                else:
+                    entry_dict[t1[0]] = t1[1]
             else:
-                m = m + '"Date": '
-            m += '"' + t1[0] + '"'
-    m+="}"
-    #print(m)
-    client.publish('v1/devices/me/telemetry', m, 1)
+                if t1[0].find(':') != -1:
+                    # m = m + '"Hour": '
+                    entry_dict["Hour"] = t1[0]
+                else:
+                #     m = m + '"Date": '
+                # m += '"' + t1[0] + '"'
+                    entry_dict["Date"] = t1[0]
+
+        # m+="}"
+        print(json.dumps(entry_dict))
+        client.publish('v1/devices/me/telemetry', json.dumps(entry_dict), 1)
     time.sleep(5)
-
-
-
-
